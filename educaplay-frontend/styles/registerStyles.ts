6f1c998adec89666901e3a8db530a3a53e3f88e2
Anthony3043 <am1662847@gmail.com>
2026-05-30 23:47:05 -0300

    paddingBottom logoSide register: 25

--- educaplay-frontend/styles/registerStyles.ts
+++ educaplay-frontend/styles/registerStyles.ts
@@ -67,7 +67,7 @@ export const registerStyles = StyleSheet.create({
   logoSide: {
     flex: 1,
     justifyContent: "center",
-    paddingBottom: 20,
+    paddingBottom: 25,
   },
   mascoteSide: {
     alignItems: "flex-end",
