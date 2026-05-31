131a563ccac299ae35b6c93184c3e63c0f8d0280
Anthony3043 <am1662847@gmail.com>
2026-05-30 23:43:41 -0300

    paddingTop topArea login: 29

--- educaplay-frontend/styles/loginStyles.ts
+++ educaplay-frontend/styles/loginStyles.ts
@@ -16,7 +16,7 @@ export const loginStyles = StyleSheet.create({
     width: '100%',
     alignItems: 'center',
     backgroundColor: Colors.surface,
-    paddingTop: 20,
+    paddingTop: 29,
     paddingBottom: 0,
   },
 
